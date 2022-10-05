ab550c981397ba4fd06c1b407fcb6ef616d1c2a5
Farhad Zamani <zmnfrhd@gmail.com>
2022-10-05 15:18:43 +0200

    initial commit

--- src/test_kmer.py
+++ src/test_kmer.py
@@ -2,5 +2,20 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_me():
-    assert False, "Don't forget to write tests"
+from kmer import (
+    kmer,
+    unique_kmers,
+    count_kmers
+)
+
+def test_kmer():
+    assert kmer('agtagtcg', 3) == ['agt', 'gta', 'tag', 'agt', 'gtc', 'tcg']
+    assert kmer('agtagtcg', 4) == ['agta', 'gtag', 'tagt', 'agtc', 'gtcg']
+    assert kmer('agtagtcgacg', 5) == ['agtag', 'gtagt', 'tagtc', 'agtcg', 'gtcga', 'tcgac', 'cgacg']
+
+def test_unique_kmers():
+    assert unique_kmers('agtagtcg', 3) == ['agt', 'gta', 'tag', 'gtc', 'tcg']
+    assert unique_kmers('agtagtag', 4) == ['agta', 'gtag', 'tagt']
+
+def test_count_kmers():
+    assert count_kmers('agtagtag', 4) == {'agta':2, 'gtag':2, 'tagt':1}
